fa59f0bb461f62cbdd22a0848d04e21c31e7ba1c
Kim Yo Han <64533351+rladygks329@users.noreply.github.com>
2021-08-20 21:27:59 +0900

    Update two_sum.cpp

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
-        //정답을 위한 백터 선언
-        vector<int> solution;
-
-        //두번의 for문으로 주어진 배열에서 있는 모든 경우의 수를 찾기 위함
-        //i = 1, j=2,3,4,5,6,7,8...end
-        //i = 2, j=3,4,5,6,7,8,,end...
-        //i = 3, j=4,5,6,7,8...end ...
-        //i = end-1, j = end for문끝
-        for(int i=0;i<nums.size()-1;i++)
-        {
-            for(int j=i+1;j<nums.size();j++)
-            {
-                if(nums[i]+nums[j] == target)
-                {
-                    solution.push_back(i);
-                    solution.push_back(j);
-                    return solution;
-                    //정답이 맞으면 두개의 루프를 탈풀하기 위해 break말고 return을쓴다
-                }
+        vector<int> answer;
+        unordered_map<int, int> m;
+        for(int i =0; i<nums.size();i++){
+            if(m.find(nums[i]) != m.end()){
+                answer.push_back(m[nums[i]]);
+                answer.push_back(i);
+                return answer;
             }
+            m[target - nums[i]] = i;
         }
-        return solution;
+        return answer;
     }
 };
