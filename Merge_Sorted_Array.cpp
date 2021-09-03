c9904fa545703749e75447ebd40e5943bcf833a5
Kim Yo Han <64533351+rladygks329@users.noreply.github.com>
2021-09-03 23:33:55 +0900

    Create Merge_Sorted_Array.cpp

---
+++ Merge_Sorted_Array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if(n == 0) {return;}
+        if(m == 0) {
+            nums1 = nums2;
+            return;
+        }
+        while(m > 0 && n > 0)
+        {
+            if(nums1[m-1] > nums2[n-1]){
+                nums1[m+n-1] =nums1[m-1];
+                m--;
+            }else{
+                nums1[m+n-1] = nums2[n-1];
+                n--;
+            }
+        }
+        if(n){
+            for(int i=0;i<n;i++){
+                nums1[i] = nums2[i];
+            }
+        }
+    }
+};
