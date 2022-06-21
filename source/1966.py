62d926b48dc52999e415849ae20df763ee30a83d
Kim Yo Han <rladygks329@gmail.com>
2022-06-21 16:16:07 +0900

    Update 1966.py

--- source/1966.py
+++ source/1966.py
@@ -12,7 +12,7 @@ for i in range(n):
     count = 0
     while(not result):
         x = False
-        #자신보다 중요한게 있는지 검사
+        # 자신보다 중요한게 있는지 검사
         for i in arr:
             if(i > arr[0]):
                 x = True
@@ -20,19 +20,16 @@ for i in range(n):
 
         tmp = arr.popleft()
 
-        if (x): #중요한게 있으므로 빼서 옆에 넣는다.
+        if (x):  # 중요한게 있으므로 빼서 옆에 넣는다.
             arr.append(tmp)
             if (target == 0):
                 target = len(arr)-1
             else:
                 target -= 1
-        else: # 중요한게 없으므로 빼기만 한다.
+        else:  # 중요한게 없으므로 빼기만 한다.
             count += 1
             if (target == 0):
                 result = True
             else:
                 target -= 1
     print(count)
-
-
-
