c647ebec2f27f4346c6c9bb9dbcc24570cdd3640
kim <rladygks329@gmail.com>
2022-11-27 01:39:13 +0900

    refactor: fibonachi

--- source/11726.py
+++ source/11726.py
@@ -2,15 +2,12 @@ import sys
 
 n = int(sys.stdin.readline())
 
-ans = 0
-dp = [0 for i in range(n+1)]
-if(n > 0):
-    dp[1] = 1
-if(n > 1):
-    dp[2] = 2
-for i in range(3, n+1):
-    dp[i] = dp[i-1] + dp[i-2]
+if(n < 3):
+    print(n)
+    sys.exit()
 
-ans = dp[n]
+a, b = 1, 2
+for i in range(n-2):
+    a, b = b, a+b
 
-print(ans % 10007)
+print(b % 10007)
