a2faf2af41bde8405ec45fd65fcfe19db09b3b89
Kim Yo Han <rladygks329@gmail.com>
2023-05-16 17:41:44 +0900

    refactor 18870

--- source/18870.py
+++ source/18870.py
@@ -1,18 +1,5 @@
 import sys
 n = int(sys.stdin.readline())
 data = list(map(int, sys.stdin.readline().split()))
-s = [(data[i], i) for i in range(n)] # value, index
-s.sort()
-
-prev = s[0][0]
-cur = 0
-for i in range(n):
-    value, index = s[i]
-    if prev != value:
-        cur += 1
-    data[index] = cur
-    prev = value
-
-for i in data:
-    print(i, end=" ")
-print()
+dict = {x: i for i, x in enumerate(sorted(set(data)))}
+print(" ".join(map(str, [dict[i] for i in data])))
