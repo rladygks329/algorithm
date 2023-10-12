2a2d23993d7d04c5156313f1cce826d8b6a1213d
Kim Yo Han <rladygks329@gmail.com>
2023-10-13 00:50:36 +0900

    solve 9527

--- baekjoon/9527.py
+++ baekjoon/9527.py
@@ -3,60 +3,21 @@ import sys
 s, e = map(int, sys.stdin.readline().strip().split())
 dp = [0] * 66
 
-for i in range(60):
+for i in range(1, 60):
     dp[i] = (i * 2**(i-1)) + 1
 dp[0] = 1
 
-#
-# for i in range(1, 65):
-#     dp[i] = dp[i-1] + i.bit_count()
-# print(dp)
-
-
-def helper(n):
+def solve(n):
     n = list(str(bin(n).replace('0b', '')))
+
     size = len(n)
     result = 0
-    print(n)
+    count = 0
+
     for i in range(size):
         if n[i] == '1':
-            result += dp[size - i - 1]
-
+            result += dp[size - i - 1] + 2**(size - i - 1) * count
+            count += 1
     return result
 
-answer = []
-for i in range(1, 20):
-    answer.append(helper(i))
-print(dp)
-print(answer)
-'''
-[0, 1, 2, 4, 5, 7, 9, 12, 13, 15, 17, 20, 22, 25, 28, 32, 33]
-
-[0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1]
-
-
-
-10000
-->
-0000 1111 1이 4개네?
-0001 1110 1이 4개네?
-4비트로 만들수 있는 수 
--> 1111 까지 1의 합 : (4 * 16)/2
--> 16 까지의 합 : (4 * 16)/2+ 1 (10000)
--> 2^n 까지의합 : n*(2^n)/2 + 1
-
-12 = 1100
-1000
-+ 100
-
-15까지의 합 => 4 * 16 / 2 =? 32?
-0000 1111
-0001 1110
-0010 1101
-0011 1100
-0100 1011
-.
-.
-.
-
-'''
+print(solve(e) - solve(s-1))
